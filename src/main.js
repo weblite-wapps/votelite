b60091fef3d117aa80c829ea90f296bfb6281312
ali <aliasgary73@gmail.com>
2018-03-31 13:26:08 +0430

    add css and ...

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
+// global css style
+import './helper/style/reset.css'
+import './helper/style/main.css'
+import './helper/style/global.css'
+// components
 import App from './App.vue'
 
-Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App)
-}).$mount('#app')
+Vue.config.productionTip = false
+new Vue({ render: h => h(App) }).$mount('#app')
