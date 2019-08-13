35a0e4c8506edcc0c6e54eac55514d2dc0fc338e
sadegh <mse737.khavari@gmail.com>
2019-08-13 18:34:58 +0430

    Format all js files with prettier

--- src/helper/functions/helperFunctions.js
+++ src/helper/functions/helperFunctions.js
@@ -1,24 +1,27 @@
 import * as R from 'ramda'
 
-export const findVote = (votes, id) => R.reduce((acc, item) =>
-    R.find(R.propEq("id", id), item) ?
-        { index: acc.cnt, cnt: acc.cnt + 1 } :
-        { index: acc.index, cnt: acc.cnt + 1 },
-    { index: -1, cnt: 0 })(votes)
-
-
+export const findVote = (votes, id) =>
+  R.reduce(
+    (acc, item) =>
+      R.find(R.propEq('id', id), item)
+        ? { index: acc.cnt, cnt: acc.cnt + 1 }
+        : { index: acc.index, cnt: acc.cnt + 1 },
+    { index: -1, cnt: 0 },
+  )(votes)
 
 // R.reduce((acc, item) =>
 //     R.includes(id, item) ? { index: acc.cnt, cnt: acc.cnt + 1 } : { index: acc.index, cnt: acc.cnt + 1 }
 //     , { index: -1, cnt: 0 })(votes)
-export const findVoteLength = R.reduce((acc, item) => acc.concat(R.length(item))
-    , [])
+export const findVoteLength = R.reduce(
+  (acc, item) => acc.concat(R.length(item)),
+  [],
+)
 
+export const getMembers = arr =>
+  R.reduce((acc, item) => acc.concat(item), [])(arr)
 
-export const getMembers = (arr) => R.reduce((acc, item) => acc.concat(item), [])(arr)
-
-
-export const getChartData = (votes, choices) => R.compose(
+export const getChartData = (votes, choices) =>
+  R.compose(
     R.prepend(['choices', 'number of votes']),
     choices => choices.map((choice, index) => [choice, votes[index].length]),
-)(choices)
+  )(choices)
